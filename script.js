f21618d0d916a3b88efad9f53fc4a794608baa43
jonas <jonas_schwartz@hotmail.com>
2022-01-24 08:55:56 +0100

    commit

--- script.js
+++ script.js
@@ -1,85 +1,122 @@
-const form = document.querySelector('#todoForm')
-const input = document.querySelector('#todoInput')
-const output = document.querySelector('#output')
+const form = document.querySelector('#Form');
+const input = document.querySelector('#input');
+const output = document.querySelector('#output');
 
- let todos = [];
 
- const fetchToDo = async() => {
-     const res = await fetch (`https://jsonplaceholder.typicode.com/todos`)
-     const data = await res.json()
-    
-     todos = data;
-     
-     listToDos();
-   
- }
-fetchToDo(); 
+let todoList1 = [];
+let todoList = [];
 
-const listToDos = () => {
-    output.innerHTML = '';
-    todos.forEach(todo => {
-    output.appendChild(createToDoElement(todo))
-    })
+const fetchTodos = async () => {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await resp.json()
+  todoList1 = data;
+
+  todoList = todoList1.slice(0, 10);
+  showList();
 }
 
-const createToDoElement = todo => {
-    let card = document.createElement('div');
-    card.classList.add('todo')
+fetchTodos ();
 
-    let title = document.createElement('p')
-    title.classList.add('todo-title')
-    title.innerText = todo.title
-
-    let button = document.createElement('button')
-    button.classList.add('btn','btn-danger','btn-sm')
-    button.innerText = 'X';
-
-    card.appendChild(title)
-    card.appendChild(button)
-
-button.addEventListener('click', () => removeTodo (todo.id, card))
-
-    return card;
-}
-
-function removeTodo(id,todo) {
-
-   todos = todos.filter(todo => todo.id !== id)
-   
-   todo.remove()
-
-    
-}
-
-const createNewTodo = title => {
-    fetch('https://jsonplaceholder.typicode.com/todos', {
-        method:'post',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify({
-            title,
-            completed:false
-        })
-    })
-
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        todos.unshift(data)
-        output.appendChild(createToDoElement(data))
-    })
+const showList = () => {
+  output.innerHTML = ''
+  todoList.forEach(listItem => {
+    crateListItem(listItem)
+  })
 }
 
 
+const crateListItem = item => {
+
+  let card = document.createElement('div');
+  card.classList.add('list-item');
+
+  let chGroup = document.createElement('label');
+  chGroup.classList.add('custom-checkbox');
+
+  let chBox = document.createElement('input');
+  chBox.type = 'checkbox';
+  chBox.name = 'chBox';
+  chBox.id = item.id ;
+  chBox.checked = item.completed;
+
+  let chMark = document.createElement('span');
+  chMark.classList.add('checkmark');
+  
+  let titleText = document.createElement('h4');
+  titleText.classList.add('list-item-txt');
+  titleText.innerText = item.title;
+  
+  let bttn = document.createElement('button');
+  bttn.classList.add('btn', 'btn-del');
+  if (chBox.checked !== true) {
+    bttn.classList.add('visi-btn');
+  }
+  bttn.innerText = 'DEL';
 
 
+  bttn.addEventListener('click', () => {
+    if (chBox.checked === true) {
+      removeItem(item.id)
+    }
+  })
 
-form.addEventListener('submit',e => {
-    e.preventDefault();
-if(input.value !== '') {
+
+  chGroup.append(chBox, chMark);
+  card.append(chGroup, titleText, bttn);
+  output.appendChild(card);
+}
+
+function removeItem(id) {
+  todoList = todoList.filter(item => item.id !== id)
+  showList()
+ 
+  
+}
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if(input.value !== '') {
+    createNewItem(input.value);
     input.value = '';
     input.focus()
+  }
+})
+
+const createNewItem = title => {
+  fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify({
+      title,
+      completed: false
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    data.id = Date.now().toString();
+    data.userId = 1;
+    todoList.unshift(data);
+    showList()
+    
+  })
 }
 
-}) 
+
+
+output.addEventListener('change', e => {
+  if (e.target.checked) {
+    
+    todoList.find(item => item.id == e.target.id).completed = true;
+    
+  }
+  else {
+    
+    todoList.find(item => item.id == e.target.id).completed = false;
+    
+  }
+  showList()
+});
+
